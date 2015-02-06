9857faeaf4639c21739a1ba50f0ca887ce1c52da
malar19 <malar.v1905@gmail.com>
2015-02-06 14:09:28 +0530

    Version 1-displayservlet

--- src/example/servlet/DisplayServlet.java
+++ src/example/servlet/DisplayServlet.java
@@ -40,11 +40,24 @@ public class DisplayServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<html>");
         out.println("<head>");
+        out.println("<style>");
+        out.println("table, th , td  {");
+        out.println("    border: 1px solid grey;");
+        out.println("border-collapse: collapse;");
+        out.println("     padding: 5px;");
+        out.println("}");
+        out.println("table tr:nth-child(odd)	{");
+        out.println("background-color: #f1f1f1;}");
+        out.println("table tr:nth-child(even) {");
+        out.println("background-color: #ffffff;}");
+        out.println("</style>");
+        out.println("<script src= \"http://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js\"></script>\n");
         out.println("</head>");
         out.println("<body bgcolor=\"#d3d3d3\">");
-        out.println("<br><h2><center>TEST CASE DATABASE</center></h2><br><hr><br><br><br><br>");
-        out.println("<table border=\"1px\" cols=\"7\" align=\"center\" cellpadding=\"10px\">");
+        //out.println("<br><h2><center>TEST CASE DATABASE</center></h2><br><hr><br><br><br><br>");
+        out.println("<table border=\"1px\" align=\"center\" cellpadding=\"10px\">");
         out.println("<tr>");
+        out.println("<td></td>");
         out.println("<td><b>TEST ID</b></td>");
         out.println("<td><b>DESCRIPTION</b></td>");
         out.println("<td><b>TAGS</b></td>");
@@ -53,24 +66,26 @@ public class DisplayServlet extends HttpServlet {
         out.println("<td><b>RESULT</b></td>");
         out.println("<td><b>DATE</b></td>");
         out.println("</tr>");
+        out.println("<p><input type=\"text\" ng-model=\"test\"></p>");
         try {
 
             Statement s = con.createStatement();
             ResultSet r = s.executeQuery("select * from test;");
             while (r.next()) {
                 out.println("<tr>");
-                out.println("<td>" + r.getInt(1) + "</td>");
-                out.println("<td>" + r.getString(2) + "</td>");
-                out.println("<td>" + r.getString(3) + "</td>");
-                out.println("<td>" + r.getString(4) + "</td>");
-                out.println("<td>" + r.getString(5) + "</td>");
-                out.println("<td>" + r.getBoolean(6) + "</td>");
+                out.println("<td><input type=\"radio\" id=\"val\" name=\"del\" value='"+r.getInt(1)+"'></td>");
+                out.println("<td>" + r.getInt(1) + "| filter:test </td>");
+                out.println("<td>" + r.getString(2) + "| filter:test </td>");
+                out.println("<td>" + r.getString(3) + "| filter:test </td>");
+                out.println("<td>" + r.getString(4) + "| filter:test </td>");
+                out.println("<td>" + r.getString(5) + "| filter:test </td>");
+                out.println("<td>" + r.getBoolean(6) + "| filter:test </td>");
                 out.println("<td>" + r.getDate(7) + " " + r.getTime(7) + "</td>");
                 out.println("</tr>");
             }
             out.println("</table>");
-            out.println("<br><br><center><a href=\"welcome.html\"><input type=\"button\" value=\"Back to main page.\"></a></center>");
             out.println("</form>");
+            //out.println("<input type=\"button\" id=\"check\" value=\"check\" onclick=\"check()\">");
             out.println("</body>");
             out.println("</html>");
         }
